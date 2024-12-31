89070b0a1c2b3681607fbbcadc425e580e225d05
Bhupendra Maurya <bpmaurya24th@gmail.com>
2025-01-01 00:47:45 +0530

    router and controller for user

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Server is OK" });
 });
+
+// user router
+app.use('/api/users',userRouter)
 
 // Global error handler
 app.use(globalErrorHandler);
