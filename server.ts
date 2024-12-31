2f6fc81fe35730d8ac019b607ac1329101c8c123
Bhupendra Maurya <bpmaurya24th@gmail.com>
2024-12-31 23:12:18 +0530

    server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import app from "./src/app";
-const PORT = 3000
+import { config } from "./src/config/config";
+import connectDB from "./src/config/db";
+const port = config.port || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port http://localhost:${PORT}`);
+app.listen(port, async () => {
+  console.log(`Server is listening on port http://localhost:${port}`);
+  await connectDB();
 });
