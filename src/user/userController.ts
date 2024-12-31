0cd2adb8f5738019627770f6fb216f69c0807d9b
Bhupendra Maurya <bpmaurya24th@gmail.com>
2025-01-01 01:17:27 +0530

    bcrypt  for password hash

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // Validation
-  console.log(req.body);
+  //   console.log(req.body);
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
@@ -16,7 +17,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "User already exist with this email");
     return next(error);
   }
-  res.json(req.body);
+
+  const hashPassword = await bcrypt.hash(password,10)
+    res.json(req.body);
 };
 
 export { createUser };
